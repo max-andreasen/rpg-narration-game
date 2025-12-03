1caf75978321cc4ec9af8164e547b977579b9c6f
Jarl Stephansson <jarl.stephansson@gmail.com>
2025-12-03 15:59:38 +0100

    fixed world_model and added possibility to add context

--- backend/models/world_model.py
+++ backend/models/world_model.py
@@ -6,10 +6,10 @@ from db.mongodb import *
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from dotenv import load_dotenv
-from models.rag import * # how does it work with the retriver var? 
+from models.rag import * 
 
 load_dotenv(override=True)
-
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 
 class WorldModel:
 
@@ -20,7 +20,7 @@ class WorldModel:
             "You are a mysterious LORE expert capable of answering user questions about the world"
         )
 
-    def build_prompt(self, session_id: str, user_message: str, n_turns : int = 5):
+    def build_prompt(self, session_id: str, user_message: str, n_turns : int = 5, context: str = "") -> list:
         """
         Builds the final list of messages for the LLM call.
         
@@ -34,8 +34,12 @@ class WorldModel:
         
         turns = get_turns(session_id, limit=n_turns)
         for turn in turns:
-            messages.append(SystemMessage(content=f"NARRATOR: {turn['narration']}"))
+            messages.append(SystemMessage(content=f"NARRATOR: {turn['narration']}")) # Only add the narrator part (user action not needed )
             rag_query_parts.append(f"NARRATOR: {turn['narration']}")
+        
+        if context:
+            messages.append(SystemMessage(content=f"ADDITIONAL CONTEXT: {context}")) # to add extra context if needed
+            rag_query_parts.append(f"ADDITIONAL CONTEXT: {context}")
 
         messages.append(HumanMessage(content=user_message))
         rag_query_parts.append(f"USER QUESTION: {user_message}")
@@ -55,16 +59,9 @@ class WorldModel:
             print(f"Error during RAG call in build_prompt: {e}. Proceeding without context.")
         return messages
 
-    def generate(self, session_id: str, question: str) -> str:
-        prompt = self.build_prompt(session_id, question, n_turns=5)
-        prompt = "\n".join(prompt) # unlistify
+    def generate(self, session_id: str, question: str, context: str = "") -> str: 
+        ## USUAGE: 1 - create WorldModel instance, 2 - call generate(session_id, question) to get response (pass context if needed)
+        prompt = self.build_prompt(session_id, question, n_turns=5, context=context)
         ai_message = self.model.invoke(prompt)
         content = ai_message.content
-
-        try:
-            next_turn_idx = get_next_turn_index(session_id)
-            # next up is how we store the messege history
-
-        except Exception as e:
-            print(f"Error saving to DB: {e}")
         return content
